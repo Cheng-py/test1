4b3e608b092e07a33e1799596b17becd4a0d83fb
Cheng <2238390982@qq.com>
2020-08-24 10:26:18 +0800

    ceshi

---
+++ test.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x=str(x)
+        a=x
+        if (a[::-1]==x):
+            print(True)
+        else:
+            print(False)
+
+solution = Solution()
+a=121
+solution.isPalindrome(a)
